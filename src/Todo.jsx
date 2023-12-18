4d47590268f53411accf334de1147caf9b48b68a
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-12-18 21:29:56 +0900

    完了ボタン機能の実装

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,6 +21,15 @@ function Todo() {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="wrapper">
@@ -41,7 +50,7 @@ function Todo() {
                 <li key={todo}>
                   <div className="list-row">
                     <p className="todo-item">{todo}</p>
-                    <button>完了</button>
+                    <button onClick={() => onClickComplete(index)}>完了</button>
                     <button onClick={() => onClickDelete(index)}>削除</button>
                   </div>
                 </li>
